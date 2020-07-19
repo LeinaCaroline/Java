5894b982d0a4116af72707cf90c2f564b08ae547
Leina Caroline <50548164+LeinaCaroline@users.noreply.github.com>
2020-07-18 22:54:03 -0300

    Add files via upload

---
+++ JavaBean/ControleRemoto/Televisor.java
@@ -0,0 +1,27 @@
+package Nova_lista_1;
+
+public class Televisor {
+ 
+ private int canal;
+ private int volume;
+
+ 
+public int getCanal() {
+	return canal;
+}
+public void setCanal(int c) {
+	this.canal = c;
+}
+public int getVolume() {
+	return volume;
+}
+public void setVolume(int volume) {
+	this.volume = volume;
+}
+  
+  
+	
+	
+	
+	
+}

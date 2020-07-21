401ff015062dfcdfc709800e0093f97b9b8b3c2e
Leina Caroline <50548164+LeinaCaroline@users.noreply.github.com>
2020-07-21 20:17:05 -0300

    Add files via upload

---
+++ Herança/Professor1.java
@@ -0,0 +1,39 @@
+package Aprender;
+
+public class Professor1 extends Pessoa1{
+	
+	private String departamento;
+	 private String nomeCurso;
+	 private float salario;
+	
+	
+	
+
+	 public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+	public float getSalario() {
+		return salario;
+	}
+
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+	 
+	
+	 
+	 
+}

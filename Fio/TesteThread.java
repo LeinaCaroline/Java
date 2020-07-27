339b87112c9d2b89b457581d143d450f2c1aa8bb
Leina Caroline <50548164+LeinaCaroline@users.noreply.github.com>
2020-07-27 16:26:39 -0300

    Add files via upload

---
+++ Fio/TesteThread.java
@@ -0,0 +1,13 @@
+package Aprender;
+
+public class TesteThread {
+
+	public static void main(String[] args) {
+		MinhaThread thread = new MinhaThread("thread #1",1000);
+		MinhaThread thread2 = new MinhaThread("thread #2", 900);
+		MinhaThread thread3 = new MinhaThread("thread #3", 500);
+		
+
+	}
+
+}
